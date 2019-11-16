c404eedda185ba9f835a974aedadfb786b2cd04b
Mohamed-Achref Maiza <mohamed-achref.maiza@renault.com>
2019-11-16 01:10:14 +0100

    new version of macro soft-F1 loss averaging costs of both positive and negative class

--- utils.py
+++ utils.py
@@ -174,6 +174,34 @@ def macro_bce(y, y_hat):
     return cost
 
 
+def macro_double_soft_f1(y, y_hat):
+    """Compute the macro soft F1-score as a cost (average 1 - soft-F1 across all labels).
+    Use probability values instead of binary predictions.
+    This version uses the computation of soft-F1 for both positive and negative class
+    to get the cost for each label.
+    
+    Args:
+        y (int32 Tensor): targets array of shape (BATCH_SIZE, N_LABELS)
+        y_hat (float32 Tensor): probability matrix from forward propagation of shape (BATCH_SIZE, N_LABELS)
+        
+    Returns:
+        cost (scalar Tensor): value of the cost function for the batch
+    """
+    y = tf.cast(y, tf.float32)
+    y_hat = tf.cast(y_hat, tf.float32)
+    tp = tf.reduce_sum(y_hat * y, axis=0)
+    fp = tf.reduce_sum(y_hat * (1 - y), axis=0)
+    fn = tf.reduce_sum((1 - y_hat) * y, axis=0)
+    tn = tf.reduce_sum((1 - y_hat) * (1 - y), axis=0)
+    soft_f1_class1 = 2*tp / (2*tp + fn + fp + 1e-16)
+    soft_f1_class0 = 2*tn / (2*tn + fn + fp + 1e-16)
+    cost_class1 = 1 - soft_f1_class1 # reduce 1 - soft-f1_class1 in order to increase soft-f1 on class 1
+    cost_class0 = 1 - soft_f1_class0 # reduce 1 - soft-f1_class0 in order to increase soft-f1 on class 0
+    cost = 0.5 * (cost_class1 + cost_class0) # take into account both class 1 and class 0
+    macro_cost = tf.reduce_mean(cost) # average on all labels
+    return macro_cost
+
+
 def learning_curves(history):
     """Plot the learning curves of loss and macro f1 score 
     for the training and validation datasets.
